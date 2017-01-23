3927cc20dbe7f1e95e1bf7f9c529845887a21fb8
Jesse Bordoe <jesse.bordoe@gmail.com>
2017-01-23 12:53:08 +0100

    Project initialization

---
+++ config/environment.rb
@@ -0,0 +1,40 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/cran'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/cran_development.sqlite3'
+    #    adapter :sql, 'postgres://localhost/cran_development'
+    #    adapter :sql, 'mysql://localhost/cran_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/cran/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery do
+      development :test
+      test        :test
+      # production :smtp, address: ENV['SMTP_PORT'], port: 1025
+    end
+  end
+end
